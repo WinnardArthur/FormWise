30afdcd5b75b27674b6161e93189d1f2c77ef49e
WinnardArthur <winnardarthur7@gmail.com>
2024-04-17 09:27:08 +0000

    add selected and setter function

--- components/context/designer-context.tsx
+++ components/context/designer-context.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import { createContext, useState } from "react";
+import { Dispatch, SetStateAction, createContext, useState } from "react";
 import { FormElementInstance } from "../form-elements";
 
 interface DesignerContextType {
   elements: FormElementInstance[];
   addElement: (index: number, element: FormElementInstance) => void;
   removeElement: (id: string) => void;
+
+  selectedElement: FormElementInstance | null;
+  setSelectedElement: Dispatch<SetStateAction<FormElementInstance | null>>;
 }
 
 export const DesignerContext = createContext<DesignerContextType | null>(null);
@@ -17,6 +20,8 @@ export default function DesignerContextProvider({
   children: React.ReactNode;
 }) {
   const [elements, setElements] = useState<FormElementInstance[]>([]);
+  const [selectedElement, setSelectedElement] =
+    useState<FormElementInstance | null>(null);
 
   // Add an element
   const addElement = (index: number, element: FormElementInstance) => {
@@ -33,7 +38,15 @@ export default function DesignerContextProvider({
     setElements((prev) => prev.filter((element) => element.id !== id));
   };
   return (
-    <DesignerContext.Provider value={{ elements, addElement, removeElement }}>
+    <DesignerContext.Provider
+      value={{
+        elements,
+        addElement,
+        removeElement,
+        selectedElement,
+        setSelectedElement,
+      }}
+    >
       {children}
     </DesignerContext.Provider>
   );
