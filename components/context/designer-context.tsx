73bbabe239fe80c846d5ee3ab3e32d58c5bfc961
WinnardArthur <winnardarthur7@gmail.com>
2024-04-17 08:15:30 +0000

    create context provider component

---
+++ components/context/designer-context.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { createContext, useState } from "react";
+import { FormElementInstance } from "../form-elements";
+
+interface DesignerContextType {
+  elements: FormElementInstance[];
+  addElement: (index: number, element: FormElementInstance) => void;
+  removeElement: (id: string) => void;
+}
+
+export const DesignerContext = createContext<DesignerContextType | null>(null);
+
+export default function DesignerContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [elements, setElements] = useState<FormElementInstance[]>([]);
+
+  // Add an element
+  const addElement = (index: number, element: FormElementInstance) => {
+    setElements((prev) => {
+      const newElements = [...prev];
+      newElements.splice(index, 0, element);
+
+      return newElements;
+    });
+  };
+
+  // Remove an element
+  const removeElement = (id: string) => {
+    setElements((prev) => prev.filter((element) => element.id !== id));
+  };
+  return (
+    <DesignerContext.Provider value={{ elements, addElement, removeElement }}>
+      {children}
+    </DesignerContext.Provider>
+  );
+}
